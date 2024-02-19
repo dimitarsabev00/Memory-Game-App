3d913aba689b9c231c8281eca56c545397a9d67a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-19 17:28:58 +0200

    Added handleShuffleCards Functionality In App.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { cardImages } from "./utils/constants";
 import "./App.css";
-
+interface Card {
+  id: number;
+  src: string;
+}
 const App: React.FC = () => {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [turns, setTurns] = useState(0);
+
+  const handleShuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  };
   return (
     <div className="App">
       <h1>Memory Game App</h1>
-      <button>New Game</button>
+      <button onClick={handleShuffleCards}>New Game</button>
     </div>
   );
 };
