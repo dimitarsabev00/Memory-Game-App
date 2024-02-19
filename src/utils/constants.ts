1ba3e76ff9ff5f252827518ec06a9ea9d317dd0a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-19 17:57:05 +0200

    Fix Bug With src Value For cardImages Array In constants.ts File In utils Folder

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,20 +1,20 @@
 export const cardImages = [
   {
-    src: "./assets/images/helmet-1.png",
+    src: "../src/assets/images/helmet-1.png",
   },
   {
-    src: "./assets/images/potion-1.png",
+    src: "../src/assets/images/potion-1.png",
   },
   {
-    src: "./assets/images/ring-1.png",
+    src: "../src/assets/images/ring-1.png",
   },
   {
-    src: "./assets/images/scroll-1.png",
+    src: "../src/assets/images/scroll-1.png",
   },
   {
-    src: "./assets/images/shield-1.png",
+    src: "../src/assets/images/shield-1.png",
   },
   {
-    src: "./assets/images/sword-1.png",
+    src: "../src/assets/images/sword-1.png",
   },
 ];
