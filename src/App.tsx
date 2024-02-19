1ba86ec389217e4b18223d62f37353872966fbb1
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-19 17:28:29 +0200

    Added Design For Heading & Button In App.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>Memory Game App</div>;
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Memory Game App</h1>
+      <button>New Game</button>
+    </div>
+  );
 };
 
 export default App;
